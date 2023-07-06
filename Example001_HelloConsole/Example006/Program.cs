ecae025be6a334ee9bb5a01232424507380cae6e
SohinaND <sohina.nata@gmail.com>
2023-07-06 22:33:02 +0300

    Example006

---
+++ Example001_HelloConsole/Example006/Program.cs
@@ -0,0 +1,15 @@
+﻿int a = 5;
+int b = 8;
+int c = 1;
+int d = 9;
+int e = 4;
+
+int max = a;
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (e > max) max = e;
+
+Console.Write("Max = ");
+Console.WriteLine(max);
